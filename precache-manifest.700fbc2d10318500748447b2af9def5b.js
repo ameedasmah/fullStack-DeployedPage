7a4f6b73aea10939462acf1080f50e45979e28ba
ameedasmah <ameed.asmah@hotmail.com>
2021-03-23 13:41:27 +0200

    Updates

---
+++ precache-manifest.700fbc2d10318500748447b2af9def5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa2c6c00942c28fff3578d0880f82eb",
+    "url": "/deployedPortfolio/index.html"
+  },
+  {
+    "revision": "d16250243fe91df750ba",
+    "url": "/deployedPortfolio/static/css/main.ec818708.chunk.css"
+  },
+  {
+    "revision": "e6523ec2d288736dfc21",
+    "url": "/deployedPortfolio/static/js/2.0fcf9078.chunk.js"
+  },
+  {
+    "revision": "0d379520bf19be6441d9b3fd72145865",
+    "url": "/deployedPortfolio/static/js/2.0fcf9078.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d16250243fe91df750ba",
+    "url": "/deployedPortfolio/static/js/main.f5f0d93f.chunk.js"
+  },
+  {
+    "revision": "27b7f6cd718c289d8a17",
+    "url": "/deployedPortfolio/static/js/runtime-main.f1c9f762.js"
+  }
+]);
